072e7a3c168cb268edcb70875d1cb8bb7a2904c0
James Santucci <james.santucci@gmail.com>
2018-07-05 15:36:10 -0400

    Add debug statements for auth backend

--- http4s/src/main/scala/geotrellis/server/http4s/auth/AuthenticationBackends.scala
+++ http4s/src/main/scala/geotrellis/server/http4s/auth/AuthenticationBackends.scala
@@ -44,31 +44,43 @@ object AuthenticationBackends extends LazyLogging {
         logger.warn("Signing key not changed from default. Falling back to always successful authentication")
         successful
       }
-      case signingKey => { fromSigningKey(signingKey) }
+      case signingKey => {
+        logger.debug("Constructing auth middleware from configured signing key")
+        fromSigningKey(signingKey)
+      }
     }
   }
 
-  val successful: Kleisli[OptionT[IO, ?], Request[IO], Either[String, User]] =
+  val successful: Kleisli[OptionT[IO, ?], Request[IO], Either[String, User]] = {
+    logger.debug("Using 'successful' authentication middleware -- all requests will succeed")
     Kleisli(_ => OptionT.liftF(IO(Right(User(1, "Non-authed service")))))
+  }
 
-  val failed: Kleisli[OptionT[IO, ?], Request[IO], Either[String, User]] =
+  val failed: Kleisli[OptionT[IO, ?], Request[IO], Either[String, User]] = {
+    logger.debug("Using 'failed' authentication middleware -- all requests will fail")
     Kleisli(_ => OptionT.liftF(IO(Left("oh no"))))
+  }
 
-  val fromAuthHeader: Kleisli[OptionT[IO, ?], Request[IO], Either[String, User]] = Kleisli(
-    {
-      request => {
-
-        val message = for {
-          header <- request.headers.get(Authorization).toRight("Couldn't find an Authorization header")
-          token <- {
-            if (header.value.replace("Token ", "").length >= 10)
-              Right(header.value.replace("Token ", ""))
-            else Left("Invalid token")
-          }
-          message <- Either.catchOnly[NumberFormatException](token.toLong).leftMap(_.toString)
-        } yield message
-        message.traverse(_ => OptionT.liftF(IO(User(1234, "ljaskdlfjsd"))))
+  val fromAuthHeader: Kleisli[OptionT[IO, ?], Request[IO], Either[String, User]] = {
+    logger.debug("""
+      | Using auth header-based authentication with length check --
+      | auth headers with length > 10 will succeed")
+    """.trim.stripMargin)
+    Kleisli(
+      {
+        request => {
+          val message = for {
+            header <- request.headers.get(Authorization).toRight("Couldn't find an Authorization header")
+            token <- {
+              if (header.value.replace("Token ", "").length >= 10)
+                Right(header.value.replace("Token ", ""))
+              else Left("Invalid token")
+            }
+            message <- Either.catchOnly[NumberFormatException](token.toLong).leftMap(_.toString)
+          } yield message
+          message.traverse(_ => OptionT.liftF(IO(User(1234, "ljaskdlfjsd"))))
+        }
       }
-    }
-  )
+    )
+  }
 }
