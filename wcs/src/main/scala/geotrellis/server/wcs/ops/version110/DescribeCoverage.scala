b35dabc7bcf559198e19f49e316cbf6223f5ab95
jpolchlo <jpolchlopek@azavea.com>
2018-11-12 16:51:27 -0500

    Split implementation of 1.0.0 and 1.1.0 WCS spec for readability

---
+++ wcs/src/main/scala/geotrellis/server/wcs/ops/version110/DescribeCoverage.scala
@@ -0,0 +1,85 @@
+package geotrellis.server.wcs.ops.version110
+
+import geotrellis.server.wcs.ops.{Common, MetadataCatalog}
+import geotrellis.server.wcs.ops.{DescribeCoverage => DescribeCoverageBase}
+import geotrellis.server.wcs.params.DescribeCoverageWcsParams
+
+import geotrellis.proj4._
+import geotrellis.raster._
+import geotrellis.raster.reproject.ReprojectRasterExtent
+import geotrellis.spark._
+import geotrellis.spark.io._
+import geotrellis.spark.io.json._
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.xml._
+
+object DescribeCoverage extends DescribeCoverageBase {
+  private def addDescriptions(catalog: MetadataCatalog)(identifier: String) = {
+    logger.info(s"Received v1.1.0 DescribeCoverage request for layer $identifier")
+
+    catalog(identifier)._2 match {
+      case Some(metadata) =>
+        val ex = metadata.boundsToExtent(metadata.gridBounds)
+        val crs = metadata.crs
+        val (w, h) = (metadata.gridBounds.width * metadata.tileCols, metadata.gridBounds.height * metadata.tileRows)
+        val re = RasterExtent(ex, w, h)
+        val llre = ReprojectRasterExtent(re, crs, LatLng)
+        val llex = llre.extent
+
+        <CoverageDescription>
+          <Identifier>{ identifier }</Identifier>
+          <Description>Geotrellis layer</Description>
+          <Domain>
+            <SpatialDomain>
+              { Common.boundingBox110(ex.reproject(crs, LatLng), LatLng, "ows:WGS84BoundingBox") }
+              <!-- Orginal projected extent: -->
+              { Common.boundingBox110(ex, crs) }
+              <ows:BoundingBox crs="urn:ogc:def:crs:OGC::imageCRS" dimensions="2">
+                <ows:LowerCorner>0 0</ows:LowerCorner>
+                <ows:UpperCorner>{ "%d %d".format(w, h) }</ows:UpperCorner>
+              </ows:BoundingBox>
+              <GridCRS>
+                <GridBaseCRS>{ Common.crsToUrnString(crs) }</GridBaseCRS>
+                <GridType>urn:ogc:def:method:WCS:1.1:2dGridIn2dCrs</GridType>
+                <GridOrigin>{ ({ loc: (Double, Double) => "%f %f".format(loc._1, loc._2)})(re.gridToMap(0, h - 1)) }</GridOrigin>
+                <GridOffsets>{ "%f %f".format(re.cellwidth, -re.cellheight) }</GridOffsets>
+                <GridCS>urn:ogc:def:cs:OGC:0.0:Grid2dSquareCS</GridCS>
+              </GridCRS>
+            </SpatialDomain>
+          </Domain>
+          <Range>
+            <Field>
+              <Identifier>contents</Identifier>
+              <Definition>
+                <AnyValue />
+              </Definition>
+              <InterpolationMethods>
+                <DefaultMethod>nearest neighbor</DefaultMethod>
+                <OtherMethod>bilinear</OtherMethod>
+              </InterpolationMethods>
+            </Field>
+          </Range>
+          <SupportedCRS>{ Common.crsToUrnString(LatLng) }</SupportedCRS>
+          <SupportedFormat>image/tiff</SupportedFormat>
+        </CoverageDescription>
+
+      case None =>
+        val comment = <!-- -->
+        comment.copy(commentText = s"No metadata available for $identifier")
+    }
+  }
+
+  def build(metadata: MetadataCatalog, params: DescribeCoverageWcsParams): Elem = {
+    logger.info("BUILDING COVERAGE", metadata, params)
+    <CoverageDescriptions xmlns="http://www.opengis.net/wcs/1.1"
+                          xmlns:ows="http://www.opengis.net/ows"
+                          xmlns:xlink="http://www.w3.org/1999/xlink"
+                          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                          xmlns:ogc="http://www.opengis.net/ogc"
+                          xmlns:gml="http://www.opengis.net/gml"
+                          version="1.1.0">
+      { params.identifiers.map(addDescriptions(metadata)(_)) }
+    </CoverageDescriptions>
+  }
+}
---
+++ wcs/src/main/scala/geotrellis/server/wcs/ops/version110/GetCapabilities.scala
@@ -0,0 +1,84 @@
+package geotrellis.server.wcs.ops.version110
+
+import geotrellis.server.wcs.ops.{Common, MetadataCatalog}
+import geotrellis.server.wcs.ops.{GetCapabilities => GetCapabilitiesBase}
+import geotrellis.server.wcs.params.GetCapabilitiesWcsParams
+
+import geotrellis.spark._
+import geotrellis.spark.io._
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.util.Try
+import scala.xml._
+
+object GetCapabilities extends GetCapabilitiesBase {
+  // Cribbed from https://github.com/ngageoint/mrgeo/blob/master/mrgeo-services/mrgeo-services-wcs/src/main/java/org/mrgeo/services/wcs/WcsCapabilities.java
+
+  private def makeElement(requestURL: String, operation: String) = {
+    <ows:Operation name={ operation }>
+      <ows:DCP>
+        <ows:HTTP>
+          <ows:Get xlink:href={ requestURL }></ows:Get>
+          <ows:Post xlink:href={ requestURL }></ows:Post>
+        </ows:HTTP>
+      </ows:DCP>
+    </ows:Operation>
+  }
+
+  private def addLayers(metadata: MetadataCatalog) = {
+    metadata.map { case (identifier, (zooms, maybeMetadata)) => {
+      logger.info(s"Adding v1.1.0 tag for $identifier")
+      maybeMetadata match {
+        case Some(metadata) =>
+          val crs = metadata.crs
+          val ex = metadata.extent
+          <wcs:CoverageSummary>
+            <wcs:Identifier>{ identifier }</wcs:Identifier>
+            { Common.boundingBox110(ex, crs) }
+            {
+              if (crs.epsgCode.isDefined) {
+                <SupportedCRS>urn:ogs:def:crs:EPSG::{ crs.epsgCode.get.toString }</SupportedCRS>
+              }
+            }
+            <SupportedFormat>image/geotiff</SupportedFormat>
+            <SupportedFormat>image/geotif</SupportedFormat>
+            <SupportedFormat>image/tiff</SupportedFormat>
+            <SupportedFormat>image/tif</SupportedFormat>
+          </wcs:CoverageSummary>
+        case None =>
+          val comment = <!--  -->
+          comment.copy(commentText = s"Loading of $identifier failed")
+      }
+    }}
+  }
+
+  def build(requestURL: String, metadata: MetadataCatalog, params: GetCapabilitiesWcsParams): Elem = {
+    // Pulled example template from http://nsidc.org/cgi-bin/atlas_north?service=WCS&request=GetCapabilities&version=1.1.1
+    val version = params.version.split('.')
+    <wcs:Capabilities xmlns:wcs={"http://www.opengis.net/wcs/" + version(0) + "." + version(1)}
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                      xmlns:ogc="http://www.opengis.net/ogc"
+                      xmlns:ows="http://www.opengis.net/ows"
+                      xmlns:gml="http://www.opengis.net/gml"
+                      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                      version={params.version}>
+      <ows:ServiceIdentification>
+        <ows:Title>GeoTrellis Web Coverage Service</ows:Title>
+        <ows:ServiceType>OGS WCS</ows:ServiceType>
+        <ows:ServiceTypeVersion>1.1.0</ows:ServiceTypeVersion>
+        <ows:Fees>NONE</ows:Fees>
+        <ows:AccessConstraints>NONE</ows:AccessConstraints>
+      </ows:ServiceIdentification>
+      <ows:OperationsMetadata>
+        { makeElement(requestURL, "GetCapabilities") }
+        { makeElement(requestURL, "DescribeCoverage") }
+        { makeElement(requestURL, "GetCoverage") }
+      </ows:OperationsMetadata>
+      <ows:ServiceProvider>
+      </ows:ServiceProvider>
+      <wcs:Contents>
+        { addLayers(metadata) }
+      </wcs:Contents>
+    </wcs:Capabilities>
+  }
+}
