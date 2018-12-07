59507313d9bf8a755d3dd14ea16249de81cda8cd
Nathan Zimmerman <npzimmerman@gmail.com>
2018-12-07 12:36:26 -0500

    Organize extent handling code and improve sampling strategy

--- core/src/main/scala/geotrellis/server/LayerHistogram.scala
+++ core/src/main/scala/geotrellis/server/LayerHistogram.scala
@@ -1,5 +1,6 @@
 package geotrellis.server
 
+import geotrellis.server.extent.SampleUtils
 import HasRasterExtents.ops._
 
 import com.azavea.maml.error._
@@ -9,55 +10,14 @@ import geotrellis.vector.Extent
 import geotrellis.raster._
 import geotrellis.raster.histogram._
 
+import com.typesafe.scalalogging.LazyLogging
 import cats.data.{NonEmptyList => NEL}
 import cats.effect._
 import cats.implicits._
-import com.typesafe.scalalogging.LazyLogging
+import cats.syntax.either._
+
 
 object LayerHistogram extends LazyLogging {
-
-  /** Sample imagery based on a provided sample extent */
-  final def sampleRasterExtent(uberExtent: Extent, cs: CellSize, maxCells: Int): Extent = {
-    if (uberExtent.width <= 0 || uberExtent.height <= 0) uberExtent
-    else {
-      logger.debug(s"Finding sample extent for UberExtent $uberExtent for $cs with a maximum sample of $maxCells cells")
-      val sampleWidth = math.sqrt(maxCells.toDouble) * cs.width
-      val sampleHeight = math.sqrt(maxCells.toDouble) * cs.height
-
-      logger.debug(s"orig height: ${uberExtent.height}, width: ${uberExtent.width}")
-      logger.debug(s"ideal sample height: $sampleHeight, ideal sample width: $sampleWidth")
-      // Sanity check here - if the desired sample extent is larger than the source extent, just use the source extent
-      val newWidth = if (sampleWidth > uberExtent.width) uberExtent.width else sampleWidth
-      val newHeight = if (sampleHeight > uberExtent.height) uberExtent.height else sampleHeight
-
-      // we need to avoid doing math which *should* result in equal minimum values (floating point issues)
-      val xmin =
-        if (newWidth == uberExtent.width) uberExtent.xmin
-        else uberExtent.xmax - newWidth
-      val ymin =
-        if (newHeight == uberExtent.height) uberExtent.ymin
-        else uberExtent.ymax - newHeight
-
-      val sample = Extent(xmin, ymin, uberExtent.xmax, uberExtent.ymax)
-      logger.debug(s"The sample extent covers ${(sample.area / uberExtent.area) * 100}% of the source extent")
-      sample
-    }
-  }
-
-
-  /** Choose the largest cellsize */
-  final def chooseLargestCellSize(nativeCellSizes: NEL[CellSize]): CellSize =
-    nativeCellSizes
-      .reduceLeft({ (chosenCS: CellSize, nextCS: CellSize) =>
-        val chosenSize = chosenCS.height * chosenCS.width
-        val nextSize = nextCS.height * nextCS.width
-
-        if (nextSize > chosenSize)
-          nextCS
-        else
-          chosenCS
-      })
-
 
   case class NoSuitableHistogramResolution(cells: Int) extends Throwable
   case class RequireIntersectingSources() extends Throwable
@@ -74,25 +34,33 @@ object LayerHistogram extends LazyLogging {
              contextShift: ContextShift[IO]
   ): IO[Interpreted[List[Histogram[Double]]]] =
     for {
-      params           <- getParams
-      rasterExtents    <- NEL.fromListUnsafe(params.values.toList)
-                            .map(_.rasterExtents)
-                            .parSequence
-                            .map(_.flatten)
-      intersection     <- IO { rasterExtents.foldLeft(Option.empty[Extent])({ (mbExtent, re) =>
-                            mbExtent match {
-                              case Some(extent) =>
-                                extent.intersection(re.extent)
-                              case None =>
-                                Some(re.extent)
-                            }
-                          }).getOrElse(throw new RequireIntersectingSources()) }
-      cellSize         <- IO { chooseLargestCellSize(rasterExtents.map(_.cellSize)) }
-      sampleExtent     <- IO { sampleRasterExtent(intersection, cellSize, maxCells) }
-      mbtileForExtent  <- IO { LayerExtent(getExpression, getParams, interpreter) }
-      interpretedTile  <- mbtileForExtent(sampleExtent, cellSize)
-    } yield interpretedTile.map { mbtile =>
-      mbtile.bands.map { band => StreamingHistogram.fromTile(band) }.toList
+      params            <- getParams
+      rasterExtents     <- NEL.fromListUnsafe(params.values.toList)
+                             .map(_.rasterExtents)
+                             .parSequence
+                             .map(_.flatten)
+      intersection      <- IO { SampleUtils.intersectExtents(rasterExtents.map(_.extent))
+                                  .getOrElse(throw new RequireIntersectingSources()) }
+      cellSize          <- IO { SampleUtils.chooseLargestCellSize(rasterExtents.map(_.cellSize)) }
+      sampleExtents     <- IO { SampleUtils.sampleRasterExtent(intersection, cellSize, maxCells) }
+      mbtileForExtent   <- IO { LayerExtent(getExpression, getParams, interpreter) }
+      interpretedTileTL <- mbtileForExtent(sampleExtents._1, cellSize)
+      interpretedTileTR <- mbtileForExtent(sampleExtents._2, cellSize)
+      interpretedTileBL <- mbtileForExtent(sampleExtents._3, cellSize)
+      interpretedTileBR <- mbtileForExtent(sampleExtents._4, cellSize)
+    } yield (interpretedTileTL, interpretedTileTR, interpretedTileBL, interpretedTileBR).mapN {
+      case (mbtileTL, mbtileTR, mbtileBL, mbtileBR) =>
+        val tl = mbtileTL.bands.map { band => StreamingHistogram.fromTile(band) }.toList
+        val tr = mbtileTR.bands.map { band => StreamingHistogram.fromTile(band) }.toList
+        val bl = mbtileBL.bands.map { band => StreamingHistogram.fromTile(band) }.toList
+        val br = mbtileBR.bands.map { band => StreamingHistogram.fromTile(band) }.toList
+        List(tr, bl, br).foldLeft(tl.asInstanceOf[List[Histogram[Double]]])({ (histListAcc, histListNext) =>
+          val arr = Array[Histogram[Double]]()
+          for (idx <- 0 to histListAcc.length - 1) {
+            arr(idx) = histListAcc(idx) merge histListNext(idx).asInstanceOf[Histogram[Double]]
+          }
+          arr.toList
+        })
     }
 
   def generateExpression[Param](
---
+++ core/src/main/scala/geotrellis/server/extent/SampleUtils.scala
@@ -0,0 +1,60 @@
+package geotrellis.server.extent
+
+import geotrellis.vector.Extent
+import geotrellis.raster._
+
+import com.typesafe.scalalogging.LazyLogging
+import cats.data.{NonEmptyList => NEL}
+import cats.effect._
+import cats.implicits._
+import cats.syntax.either._
+
+
+object SampleUtils extends LazyLogging {
+
+  /** Sample imagery within the provided extent */
+  final def sampleRasterExtent(uberExtent: Extent, cs: CellSize, maxCells: Int): (Extent, Extent, Extent, Extent) = {
+    logger.trace(s"Finding sample extent for UberExtent $uberExtent for $cs with a maximum sample of $maxCells cells")
+    val sampleWidth = math.sqrt(maxCells.toDouble) * cs.width
+    val sampleHeight = math.sqrt(maxCells.toDouble) * cs.height
+
+    logger.trace(s"orig height: ${uberExtent.height}, width: ${uberExtent.width}")
+    logger.trace(s"ideal sample height: $sampleHeight, ideal sample width: $sampleWidth")
+    // Sanity check here - if the desired sample extent is larger than the source extent, just use the source extent
+    val widthDelta = (if (sampleWidth > uberExtent.width) uberExtent.width else sampleWidth) / 2
+    val heightDelta = (if (sampleHeight > uberExtent.height) uberExtent.height else sampleHeight) / 2
+
+    val tl = Extent(uberExtent.xmin,              uberExtent.ymax - heightDelta, uberExtent.xmin + widthDelta, uberExtent.ymax)
+    val tr = Extent(uberExtent.xmax - widthDelta, uberExtent.ymax - heightDelta, uberExtent.xmax,              uberExtent.ymax)
+    val bl = Extent(uberExtent.xmin,              uberExtent.ymin,               uberExtent.xmin + widthDelta, uberExtent.ymin + heightDelta)
+    val br = Extent(uberExtent.xmax - widthDelta, uberExtent.ymin,               uberExtent.xmax,              uberExtent.ymin + heightDelta)
+    logger.trace(s"The sample extent covers ${((tl.area + tr.area + bl.area + br.area) / uberExtent.area) * 100}% of the source extent")
+    (tl, tr, bl, br)
+  }
+
+  /** Choose the largest cellsize */
+  final def chooseLargestCellSize(nativeCellSizes: NEL[CellSize]): CellSize =
+    nativeCellSizes
+      .reduceLeft({ (chosenCS: CellSize, nextCS: CellSize) =>
+        val chosenSize = chosenCS.height * chosenCS.width
+        val nextSize = nextCS.height * nextCS.width
+
+        if (nextSize > chosenSize)
+          nextCS
+        else
+          chosenCS
+      })
+
+  final def intersectExtents(extents: NEL[Extent]): Option[Extent] = {
+    extents.tail.foldLeft(Option(extents.head))({
+      case (Some(ex1), ex2) => ex1 intersection ex2
+      case _ => None
+    })
+  }
+
+  final def unionExtents(extents: NEL[Extent]): Option[Extent] =
+    Some(extents.tail.foldLeft(extents.head)({ (ex1, ex2) => ex1 combine ex2 }))
+
+
+}
+
