e3874a1b56a69db6093c6fa550bac6ee9f5561d4
Eugene Cheipesh <echeipesh@gmail.com>
2019-03-11 16:32:21 -0400

    Fix use EPSG authority for CRS URNs in utility

--- ogc/src/main/scala/geotrellis/server/ogc/URN.scala
+++ ogc/src/main/scala/geotrellis/server/ogc/URN.scala
@@ -4,11 +4,10 @@ import geotrellis.proj4.CRS
 
 object URN {
   def fromCrs(crs: CRS): Option[String] = crs.epsgCode.map { code =>
-    s"urn:ogc:def:crs:OGC:1.3:EPSG:$code"
+    s"urn:ogc:def:crs:EPSG:9.2:$code"
   }
 
   def unsafeFromCrs(crs: CRS): String = {
-    val code = crs.epsgCode.get
-    s"urn:ogc:def:crs:OGC:1.3:EPSG:$code"
+    fromCrs(crs).get
   }
 }
