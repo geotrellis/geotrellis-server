c20165867eadb376e69bc3b2f6170b88347d5230
Rocky Breslow <rbreslow@azavea.com>
2019-07-23 10:40:55 -0400

    Publish to Sonatype Nexus via CircleCI (#138)
    * Publish to Sonatype Nexus via CircleCI
    
    * changelog
    
    * fix indentation in scripts/server
    
    * clean up config.yml

--- .travis/build-and-test-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -it --net=host \
-  -v $HOME/.ivy2:/root/.ivy2 \
-  -v $HOME/.sbt:/root/.sbt \
-  -v $TRAVIS_BUILD_DIR:/geotrellis-server \
-  -e TRAVIS_SCALA_VERSION=$TRAVIS_SCALA_VERSION \
-  -e TRAVIS_COMMIT=$TRAVIS_COMMIT \
-  -e TRAVIS_JDK_VERSION=$TRAVIS_JDK_VERSION daunnc/openjdk-gdal:2.3.2 /bin/bash -c "cd /geotrellis-server; .travis/build-and-test.sh"
--- .travis/build-and-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" \
-  "project core" test || { exit 1; }
-
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" \
-  "project example" test || { exit 1; }
