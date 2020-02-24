d04341c501dffe2e438f32874d437e8f70dbeeca
Nathan Zimmerman <npzimmerman@gmail.com>
2020-02-24 14:29:22 -0500

    Use linspace to interpolate breaks (#213)
    * Use linspace to interpolate breaks

--- ogc/src/main/scala/geotrellis/server/ogc/OgcStyle.scala
+++ ogc/src/main/scala/geotrellis/server/ogc/OgcStyle.scala
@@ -19,6 +19,7 @@ package geotrellis.server.ogc
 import geotrellis.raster._
 import geotrellis.raster.histogram.Histogram
 import geotrellis.raster.render.{ColorMap, ColorRamp}
+import geotrellis.util.np.linspace
 
 trait OgcStyle {
   def name: String
@@ -59,34 +60,45 @@ case class ColorRampStyle(
   maxRender: Option[Double],
   legends: List[LegendModel] = Nil
 ) extends OgcStyle {
+  lazy val logger = org.log4s.getLogger
+  def breaks(hists: List[Histogram[Double]], numBreaks: Int): Array[Double] = {
+    val minBreak = minRender
+      .orElse(hists.head.minValue())
+      .getOrElse {
+        logger.warn(s"No minimum render value found, using ${Double.MinValue}")
+        Double.MinValue
+      }
+    val maxBreak = maxRender
+      .orElse(hists.head.maxValue())
+      .getOrElse {
+        logger.warn(s"No maximum render value found, using ${Double.MaxValue}")
+        Double.MaxValue
+      }
+    linspace(minBreak, maxBreak, numBreaks)
+  }
+
   def renderImage(
     mbtile: MultibandTile,
     format: OutputFormat,
     hists: List[Histogram[Double]]
   ): Array[Byte] = {
+    // The number of stops between each provided break
     val numStops: Int = stops.getOrElse(colorRamp.colors.length)
 
-    val clampedBreaks =
-      mbtile.band(0)
-        .mapDouble { z =>
-          val min = minRender.getOrElse(Double.MinValue)
-          val max = maxRender.getOrElse(Double.MaxValue)
-          if (isData(z)) { if(z > max) { max } else if(z < min) { min } else { z } }
-          else { z }
-        }.histogramDouble()
-        .quantileBreaks(numStops)
+    // The colors, interpolated (invisible added for values below our minimum break)
+    val interpolatedColors: Vector[Int] =
+      0x00000000 +: colorRamp.stops(numStops)
 
-    val interpolatedBreaks =
-      Render.linearInterpolationBreaks(clampedBreaks, numStops)
+    val interpolatedBreaks: Array[Double] =
+      breaks(hists, interpolatedColors.length)
 
-    val cmap = colorRamp.toColorMap(interpolatedBreaks)
+    val cmap = ColorRamp(interpolatedColors).toColorMap(interpolatedBreaks)
+
     format match {
       case format: OutputFormat.Png =>
        format.render(mbtile.band(bandIndex = 0), cmap)
-
       case OutputFormat.Jpg =>
         mbtile.band(bandIndex = 0).renderJpg(cmap).bytes
-
       case OutputFormat.GeoTiff => ??? // Implementation necessary
     }
   }
