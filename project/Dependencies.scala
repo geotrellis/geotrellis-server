37825229e427a1dc1cb41ac1535d7c69a57a77ac
Grigory <gr.pomadchin@gmail.com>
2021-07-01 14:48:47 -0400

    Bump deps versions (#384)
    * Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,9 +19,9 @@ object Dependencies {
 
   val dispatchVer  = "0.11.3"
   val gtVer        = "3.6.0"
-  val stac4sVer    = "0.5.0-13-g35ad8d4-SNAPSHOT"
+  val stac4sVer    = "0.6.0"
   val jaxbApiVer   = "2.3.1"
-  val refinedVer   = "0.9.20"
+  val refinedVer   = "0.9.23"
   val shapelessVer = "2.3.3"
 
   val cats                 = catsVersion("core")
@@ -34,7 +34,7 @@ object Dependencies {
   val circeParser          = circeVersion("parser")
   val circeRefined         = circeVersion("refined")
   val circeJava8           = circeVersion("java8")
-  val commonsIO            = "commons-io"                             % "commons-io"                  % "2.9.0"
+  val commonsIO            = "commons-io"                             % "commons-io"                  % "2.10.0"
   val concHashMap          = "com.googlecode.concurrentlinkedhashmap" % "concurrentlinkedhashmap-lru" % "1.4.2"
   val decline              = "com.monovore"                          %% "decline"                     % "1.4.0"
   val geotrellisRaster     = "org.locationtech.geotrellis"           %% "geotrellis-raster"           % gtVer
@@ -53,17 +53,17 @@ object Dependencies {
   val http4sXml            = http4sVer("scala-xml")
   val jaxbApi              = "javax.xml.bind"                         % "jaxb-api"                    % jaxbApiVer
   val kindProjector        = "org.typelevel"                         %% "kind-projector"              % "0.13.0"
-  val semanticdbScalac     = "org.scalameta"                          % "semanticdb-scalac"           % "4.4.21"
+  val semanticdbScalac     = "org.scalameta"                          % "semanticdb-scalac"           % "4.4.23"
   val log4cats             = "io.chrisdavenport"                     %% "log4cats-slf4j"              % "1.1.1"
   val mamlJvm              = "com.azavea.geotrellis"                 %% "maml-jvm"                    % "0.6.1"
   val pureConfig           = "com.github.pureconfig"                 %% "pureconfig"                  % "0.14.1"
   val pureConfigCatsEffect = "com.github.pureconfig"                 %% "pureconfig-cats-effect"      % "0.14.1"
-  val scaffeine            = "com.github.blemale"                    %% "scaffeine"                   % "4.1.0"
+  val scaffeine            = "com.github.blemale"                    %% "scaffeine"                   % "5.0.0"
   val scalaXml             = "org.scala-lang.modules"                %% "scala-xml"                   % "1.3.0"
   val scalatest            = "org.scalatest"                         %% "scalatest"                   % "3.2.9"  % Test
   val scalacheck           = "org.scalacheck"                        %% "scalacheck"                  % "1.14.0" % Test
   val scalacheckCats       = "io.chrisdavenport"                     %% "cats-scalacheck"             % "0.1.1"  % Test
-  val sttpHttp4s           = "com.softwaremill.sttp.client3"         %% "http4s-ce2-backend"          % "3.3.6"
+  val sttpHttp4s           = "com.softwaremill.sttp.client3"         %% "http4s-ce2-backend"          % "3.3.9"
   val macrosParadise       = "org.scalamacros"                        % "paradise"                    % "2.1.1"
   val scalaParser          = "org.scala-lang.modules"                %% "scala-parser-combinators"    % "1.1.2"
   val shapeless            = "com.chuusai"                           %% "shapeless"                   % shapelessVer
